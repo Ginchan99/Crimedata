4e7393eaf7f08919bd0b5178e18331194e30e042
Ginchan99 <pg21tushti.sinha@isbmb.ac.in>
2022-09-30 13:03:20 +0530

    first day of data analytics

--- Crimedataanalysis98.R
+++ Crimedataanalysis98.R
@@ -1,3 +1,30 @@
-#gaand mara
-ghd
-dcds
+rm(list = ls())
+# type ctrl + r
+
+setwd("C:/Users/tusht/OneDrive/Desktop/CrimeDataBDA")
+mvt = read.csv("CrimeData.csv")
+# How many rows of data (observations) are in this dataset?
+length(mvt)
+length(mvt$ID)
+
+# summary of data
+
+str(mvt)
+summary(mvt)
+
+# Using the "max" function, what is the maximum value of the variable "ID"?
+mvt$LocationDescription = as.factor(mvt$LocationDescription)
+summary(mvt)
+max(mvt$ID)
+min(mvt$ID)
+mean(mvt$ID)
+
+var(mvt$ID)
+sd(mvt$ID)
+#how many observations have a locationdescription value of Alley
+summary(mvt$LocationDescription)
+#OR
+LocationDescription = table(mvt$LocationDescription)
+
+View(LocationDescription) #view as table 
+
